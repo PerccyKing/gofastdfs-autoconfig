956756d45b1f70bdbabaeeb602f1802da3456fdd
PerccyKing <perccyking@gmail.com>
2021-03-26 19:28:49 +0800

    init

---
+++ gofastdfs-spring-boot-autoconfigure/src/main/java/cn/com/pism/gfd/GoFastDsfConfiguration.java
@@ -0,0 +1,8 @@
+package cn.com.pism.gfd;
+
+/**
+ * @author PerccyKing
+ * @date 2021/3/26 19:10
+ */
+public class GoFastDsfConfiguration {
+}
---
+++ gofastdfs-spring-boot-autoconfigure/src/main/java/cn/com/pism/gfd/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author wangyihuai
+ * @date 2021/3/26 19:10
+ */
+package cn.com.pism.gfd;
