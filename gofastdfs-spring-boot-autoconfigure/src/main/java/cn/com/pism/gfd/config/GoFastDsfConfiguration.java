a31034ba7c624ac4650f27d8169013de0dba7e44
PerccyKing <perccyking@gmail.com>
2021-03-26 20:34:57 +0800

    MOD GoFastDfsProperties.java     GoFastDsfConfiguration.java     spring.factories

--- gofastdfs-spring-boot-autoconfigure/src/main/java/cn/com/pism/gfd/config/GoFastDsfConfiguration.java
+++ gofastdfs-spring-boot-autoconfigure/src/main/java/cn/com/pism/gfd/config/GoFastDsfConfiguration.java
@@ -2,11 +2,13 @@ package cn.com.pism.gfd.config;
 
 import cn.com.pism.gfd.properties.GoFastDfsProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author PerccyKing
  * @date 2021/3/26 19:10
  */
+@Configuration
 @EnableConfigurationProperties(GoFastDfsProperties.class)
 public class GoFastDsfConfiguration {
 }
--- gofastdfs-spring-boot-autoconfigure/src/main/java/cn/com/pism/gfd/properties/GoFastDfsProperties.java
+++ gofastdfs-spring-boot-autoconfigure/src/main/java/cn/com/pism/gfd/properties/GoFastDfsProperties.java
@@ -1,13 +1,11 @@
 package cn.com.pism.gfd.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author wangyihuai
  * @date 2021/3/26 20:05
  */
-@Configuration
 @ConfigurationProperties(prefix = "gofast")
 public class GoFastDfsProperties {
 }
